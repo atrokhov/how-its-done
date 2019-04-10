f746694b5c6a4eccead183ca8a192b347afd7e2c
Arthur Atrokhov <aatrokhov@gmail.com>
2019-04-10 23:47:33 +0600

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+category = Category.create(name: 'Lorem', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec felis vel lectus porta posuere. Ut condimentum lacus sit amet tempor auctor. Nulla elit augue, tempor sit amet congue nec, dignissim id lorem. Cras blandit magna eu mattis vestibulum. Donec et dictum ligula. Nunc et facilisis est. In sollicitudin ac mi sit amet gravida. Sed euismod nibh dui, id euismod orci interdum in. Sed ullamcorper ex enim, vitae cursus nibh sollicitudin in. Proin lacinia ex id dolor pulvinar condimentum. Duis eget semper lacus. Phasellus eu sollicitudin nisi. Cras rutrum, sem vitae congue egestas, diam nibh pharetra dolor, nec ultrices lacus sapien eu velit. Nunc ornare orci scelerisque ex laoreet, accumsan malesuada sapien fermentum. Sed non cursus nulla. Nullam consectetur lacus sit amet dui placerat, a efficitur ligula placerat.')
+category2 = Category.create(name: 'Lorem ipsum', description: 'Lorem ipsum ')
